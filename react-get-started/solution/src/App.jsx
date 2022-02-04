40727835294ea615a4c58a8ea30121ba716aede5
Christopher Harrison <chrhar@microsoft.com>
2022-02-04 14:29:20 -0800

    Added React content

---
+++ react-get-started/solution/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RecipeTitle from './RecipeTitle'
+import './index.css'
+
+function App() {
+    return (
+        <article>
+            <h1>Recipe Manager</h1>
+            <RecipeTitle />
+        </article>
+    )
+}
+
+export default App;
