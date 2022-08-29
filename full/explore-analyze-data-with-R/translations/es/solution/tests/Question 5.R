a63670302d996563bc277463a841fc2cc07c889e
Felipe Flores <felipe_1357@outlook.com>
2022-08-29 10:42:59 -0500

    Update full/explore-analyze-data-with-R/translations/es/solution/tests/Question 5.R
    Co-authored-by: Abril Ureña <107883174+abrilurena@users.noreply.github.com>

--- full/explore-analyze-data-with-R/translations/es/solution/tests/Question 5.R
+++ full/explore-analyze-data-with-R/translations/es/solution/tests/Question 5.R
@@ -5,7 +5,7 @@ test = list(
       hidden = FALSE,
       name = NA,
       points = 1.0,
-      success_message = "Well done! You have successfully filtered the data to include observations whose Arrival Delay falls within the 1st and 90th quantiles.",
+      success_message = "¡Bien hecho! Ha filtrado con éxito los datos para incluir observaciones cuyo retraso de llegada se encuentra dentro de los quantiles 1 y 90.",
       failure_message = "¡Casi allí! Asegúrate de haber filtrado los datos **df_flights** para incluir solo filas en las que la entrada para la columna **ArrDelay** sea mayor que **arrdelay_01pcntile** y menor que **arrdelay_90pcntile**",
       code = {
         ## Test ##
