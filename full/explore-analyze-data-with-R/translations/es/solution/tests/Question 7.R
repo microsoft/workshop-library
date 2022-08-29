cd8ec14be2b03f4388a73723b4f089b308402204
Felipe Flores <felipe_1357@outlook.com>
2022-08-29 10:50:04 -0500

    Update full/explore-analyze-data-with-R/translations/es/solution/tests/Question 7.R
    Co-authored-by: Abril Ureña <107883174+abrilurena@users.noreply.github.com>

--- full/explore-analyze-data-with-R/translations/es/solution/tests/Question 7.R
+++ full/explore-analyze-data-with-R/translations/es/solution/tests/Question 7.R
@@ -6,7 +6,7 @@ test = list(
       name = NA,
       points = 1.0,
       success_message = "¡Bien hecho! Has filtrado con éxito los datos para incluir observaciones cuyo retraso de salida se encuentra dentro de los cuantiles 1 y 90.",
-      failure_message = "Almost there! Ensure you have filtered the **df_flights** data to only include rows where the entry for **DepDelay** column is greater than **depdelay_01pcntile** and less than **depdelay_90pcntile**",
+      failure_message = "¡Casi allí! Asegúrate de haber filtrado los datos **df_flights** para incluir solo filas en las que la entrada de la columna **DepDelay** sea mayor que **depdelay_01pcntile** y menor que **depdelay_90pcntile**",
       code = {
         ## Test ##
         test_that('there are no outliers', {
