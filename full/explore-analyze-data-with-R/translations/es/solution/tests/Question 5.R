5844d3f1aab0a2be97faaccdf1dfd00d381089c7
Felipe Flores <felipe_1357@outlook.com>
2022-08-29 10:19:42 -0500

    Update full/explore-analyze-data-with-R/translations/es/solution/tests/Question 5.R
    Co-authored-by: Abril Ureña <107883174+abrilurena@users.noreply.github.com>

--- full/explore-analyze-data-with-R/translations/es/solution/tests/Question 5.R
+++ full/explore-analyze-data-with-R/translations/es/solution/tests/Question 5.R
@@ -6,7 +6,7 @@ test = list(
       name = NA,
       points = 1.0,
       success_message = "Well done! You have successfully filtered the data to include observations whose Arrival Delay falls within the 1st and 90th quantiles.",
-      failure_message = "Almost there! Ensure you have filtered the **df_flights** data to only include rows where the entry for **ArrDelay** column is greater than **arrdelay_01pcntile** and less than **arrdelay_90pcntile**",
+      failure_message = "¡Casi allí! Asegúrate de haber filtrado los datos **df_flights** para incluir solo filas en las que la entrada para la columna **ArrDelay** sea mayor que **arrdelay_01pcntile** y menor que **arrdelay_90pcntile**",
       code = {
         ## Test ##
         test_that('there are no outliers', {
